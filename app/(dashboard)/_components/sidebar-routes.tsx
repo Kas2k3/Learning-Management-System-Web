94de525f8c7ccbe5d3470a7c21d930ac04531ff5
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-04-02 11:27:39 +0700

    completed teacher mode

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
+import { usePathname } from "next/navigation";
+
 import { SidebarItem } from "./sidebar-item";
 
 const guestRoutes = [
@@ -14,10 +16,26 @@ const guestRoutes = [
         label: "Browse",
         href: "/search",
     },
+];
+
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Course",
+        href: "/teacher/courses",
+    },
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href: "/teacher/analytics",
+    },
 ]
 
 export const SidebarRoutes = () => {
-    const routes = guestRoutes;
+    const pathname = usePathname();
+
+    const isTeacherPage = pathname?.includes("/teacher");
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
     return (
         <div className="flex flex-col w-full">
