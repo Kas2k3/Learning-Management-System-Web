87e84b940971ed93f22336271aa68bb3a8b92847
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-04-10 16:36:47 +0700

    course description

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { title } from "process";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+
+interface DescriptionFormProps {
+    initialData: {
+        description: string;
+    };
+    courseId: string;
+}
+
+const formSchema = z.object({
+    description: z.string().min(1, {
+        message: "Vui lòng không để trống",
+    }),
+});
+
+export const DescriptionForm = ({
+    initialData,
+    courseId
+}: DescriptionFormProps) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const toggleEdit = () => setIsEditing((current) => !current);
+
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    });
+
+    const { isSubmitting, isValid } = form.formState;
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            await axios.patch(`/api/courses/${courseId}`, values);
+            toast.success("Đã cập nhật khóa học");
+            toggleEdit();
+            router.refresh();
+        } catch {
+            toast.error("Đã xảy ra sự cố");
+        }
+        console.log(values);
+    }
+
+    return (
+        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Mô tả khóa học
+                <Button onClick={toggleEdit} variant="ghost">
+                    {isEditing ? (
+                        <>Hủy bỏ</>
+                    ) : (
+                        <>
+                            <Pencil className="h-4 w-4 mr-2" />
+                            Chỉnh sửa mô tả
+                        </>
+                    )}
+                </Button>
+            </div>
+            {!isEditing && (
+                <p className={cn(
+                    "text=sm mt-2",
+                    !initialData.description && "text-slate-500 italic"
+                )}>
+                    {initialData.description || "Không có mô tả"}
+                </p>
+            )}
+            {isEditing && (
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4 mt-4"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="description"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Textarea
+                                            disabled={isSubmitting}
+                                            placeholder="VD: Khóa học này cung cấp cho bạn..."
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <Button
+                                disabled={!isValid || isSubmitting}
+                                type="submit"
+                            >
+                                Lưu
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            )
+            }
+        </div >
+    )
+}
