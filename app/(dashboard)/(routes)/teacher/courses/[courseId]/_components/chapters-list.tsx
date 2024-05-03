c62a764e175f55238c5e42ce7510b212bfe45bfe
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2024-05-03 21:57:04 +0700

    completed reorder chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -8,7 +8,7 @@ import {
     Draggable,
     DropResult,
 } from "@hello-pangea/dnd";
-import { Grip } from "lucide-react";
+import { Grip, Pencil } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
@@ -32,16 +32,37 @@ export const ChaptersList = ({
     }, []);
 
     useEffect(() => {
-        setChapters
+        setChapters(items);
     }, [items]);
 
+    const onDragEnd = (result: DropResult) => {
+        if (!result.destination) return;
+
+        const items = Array.from(chapters);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+
+        const startIndex = Math.min(result.source.index, result.destination.index);
+        const endIndex = Math.max(result.source.index, result.destination.index);
+
+        const updatedChapters = items.slice(startIndex, endIndex + 1);
+
+        setChapters(items);
+
+        const bulkUpdateData = updatedChapters.map((chapter) => ({
+            id: chapter.id,
+            position: items.findIndex((item) => item.id === chapter.id)
+        }));
+
+        onReorder(bulkUpdateData);
+    }
 
     if (!isMounted) {
         return null;
     }
 
     return (
-        <DragDropContext onDragEnd={() => { }}>
+        <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="chapters">
                 {(provided) => (
                     <div {...provided.droppableProps} ref={provided.innerRef}>
@@ -52,17 +73,19 @@ export const ChaptersList = ({
                                 index={index}
                             >
                                 {(provided) => (
-                                    <div className={cn(
-                                        "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md md-4 text-sm",
-                                        chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-700"
-                                    )}
+                                    <div
+                                        className={cn(
+                                            "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
+                                            chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-700"
+                                        )}
                                         ref={provided.innerRef}
                                         {...provided.draggableProps}
                                     >
-                                        <div className={cn(
-                                            "px-2 py-3 border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
-                                            chapter.isPublished && "border-r-sky-200 hover: bg-sky-200"
-                                        )}
+                                        <div
+                                            className={cn(
+                                                "px-2 py-3 border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+                                                chapter.isPublished && "border-r-sky-200 hover:bg-sky-200"
+                                            )}
                                             {...provided.dragHandleProps}
                                         >
                                             <Grip
@@ -81,17 +104,19 @@ export const ChaptersList = ({
                                                     "bg-slate-500", chapter.isPublished && "bg-sky-700"
                                                 )}
                                             >
-                                                {chapter.isPublished ? "Published" : "Draft"}
-
+                                                {chapter.isPublished ? "Đã tải lên" : "Bản nháp"}
                                             </Badge>
-
+                                            <Pencil
+                                                onClick={() => onEdit(chapter.id)}
+                                                className="w-4 h-4 cursor-pointer hover:opacity-75 transition"
+                                            />
                                         </div>
-
                                     </div>
                                 )}
                             </Draggable>
 
                         ))}
+                        {provided.placeholder}
                     </div>
                 )}
             </Droppable>
